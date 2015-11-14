b1cd21f8c4a959c390b2729b100931cf428bf6b0
aman22275 <aman.22275@gmail.com>
2015-11-15 04:23:41 +0530

    initial commit

--- app/src/main/java/com/example/singh/snap_app/MainActivity.java
+++ app/src/main/java/com/example/singh/snap_app/MainActivity.java
@@ -49,15 +49,13 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 
         ParseUser current = new ParseUser();
 
-        if(current != null) {
+        if (current != null) {
 
             //Intent intent = new Intent(this, loginActivity.class);
             //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-           // intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-           // startActivity(intent);
-        }
-        else
-        {
+            // intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            // startActivity(intent);
+        } else {
             Log.i(TAG, current.getUsername());
 
         }
@@ -107,23 +105,21 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.logout_action)
-        {
+        if (id == R.id.logout_action) {
 
             ParseUser.logOut();
-           // ParseUser currentUser = ParseUser.getCurrentUser();
+            // ParseUser currentUser = ParseUser.getCurrentUser();
             Intent intent = new Intent(this, loginActivity.class);
-             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-             startActivity(intent);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
@@ -146,37 +142,7 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 
 
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 
 }
+
